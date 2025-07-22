f80643d03bb358735b03e5d3a3d99e4de0a4ba1a
RyanStoffel <772612@calbaptist.edu>
2025-07-22 16:21:53 -0700

    Added some basic settings, such as line numbers, relative line numbers, cursor line, disabling wrap, and setting scroll off values.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -3,3 +3,11 @@ vim.cmd.colorscheme("default")
 vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 vim.api.nvim_set_hl(0, "NormalNC", { bg = "none" })
 vim.api.nvim_set_hl(0, "EndOfBuffer", { bg = "none" })
+
+-- basic settings
+vim.opt.number = true; -- line numbers
+vim.opt.relativenumber = true; -- relative line numbers
+vim.opt.cursorline = true; -- highlight the current line
+vim.opt.wrap = false -- don't wrap lines
+vim.opt.scrolloff = 10; -- keep 10 lines above/below the cursor
+vim.opt.sidescrolloff = 8; -- keep 8 columns left/right of cursor
